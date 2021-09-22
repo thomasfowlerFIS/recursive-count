e8465e9818eedbf2ee12fb9618ad0a0809462920
Thomas Fowler <thomas.fowler@flatironschool.com>
2021-09-22 12:53:34 -0600

    Initial solution and fixed a test.

--- javascript/recursive_count.js
+++ javascript/recursive_count.js
@@ -1,5 +1,14 @@
-function recursiveCount(num = 0) {
-  // type your code here
+const recursiveCount = (num = 0, arr = []) => {
+  if (num >= 0) {
+    if (arr.length === 0) {
+      let range = [...Array(num).keys()]
+      if (range.length > 0 ) console.log(range[0])
+      if (range.length > 0) recursiveCount(--num, range.slice(1))
+    } else if (arr.length > 0) {
+      console.log(arr[0])
+      if (arr.length > 0) recursiveCount(--num, arr.slice(1))
+    }
+  }
 }
 
 if (require.main === module) {
@@ -11,3 +20,5 @@ module.exports = recursiveCount;
 // OPTIONAL
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+// recursiveCount(10);
